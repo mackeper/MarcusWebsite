362a69d6669fa285ddd0034240552c014c69e520
Marcus Östling <marcus.ftw94@gmail.com>
2018-11-11 12:22:40 +0100

    add webpack configs

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -2,9 +2,6 @@ var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	//devtool: 'source-map', //Use in production
-	//devtool: 'eval-source-map', // Use in development
-
 	entry: ["babel-polyfill", __dirname + "/src/index.js"],
 	output: {
 		path: __dirname + "/build",
@@ -14,7 +11,7 @@ module.exports = {
 	module: {
 		rules: [ // Changed name from loaders
 			{
-				test: /\.(js|jsx)$/,
+				test: /\.js?$/,
 				exclude: /node_modules/,
 				use: 'babel-loader',
 			},
@@ -44,13 +41,6 @@ module.exports = {
 
 	plugins: [
 		new webpack.BannerPlugin("MarcusWebsite\n"), // Puts a string at the top of the bundled file.
-		new HtmlWebpackPlugin({template: __dirname + "/src/index.tmpl.html",}), // Generates the final html file.
+		new HtmlWebpackPlugin({template: __dirname + "/src/index.tmpl.html"}), // Generates the final html file.
 	],
-
-	devServer: {
-		contentBase: "./src",
-		historyApiFallback: true,
-		inline: true, //insert a small client entry to the bundle to refresh the page on change.
-	},
-
 }
